f4428cc93d66f35f95c3b27e1fecf48a54d0a199
Abdullah Islam <abdullahislam2701@gmail.com>
2025-03-20 00:21:52 +0500

    implented Create Post and its functionality, which enabled me to add  a new post.

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -7,7 +7,15 @@ export const PostList = createContext({
 });
 
 const postListReducer = (currPostList, action) => {
-  return currPostList;
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postID
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
 };
 
 const PostListProvider = ({ children }) => {
@@ -16,9 +24,28 @@ const PostListProvider = ({ children }) => {
     DEFAULT_POST_LIST
   );
 
-  const addPost = () => {};
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now,
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userID: userId,
+        tags: tags,
+      },
+    });
+  };
 
-  const deletePost = () => {};
+  const deletePost = (postID) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postID,
+      },
+    });
+  };
 
   return (
     <PostList.Provider value={{ postList, addPost, deletePost }}>
