4c6c5609b1f3818b623a2d39d0ec2045dd448d0f
Abdullah Islam <abdullahislam2701@gmail.com>
2025-03-21 17:59:05 +0500

    Added Welcome message,it will be visible when there are no posts left

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList } = useContext(PostListData);
   return (
     <>
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -19,10 +19,7 @@ const postListReducer = (currPostList, action) => {
 };
 
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(
-    postListReducer,
-    DEFAULT_POST_LIST
-  );
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
 
   const addPost = (userId, postTitle, postBody, reactions, tags) => {
     dispatchPostList({
@@ -53,34 +50,5 @@ const PostListProvider = ({ children }) => {
     </PostList.Provider>
   );
 };
-
-const DEFAULT_POST_LIST = [
-  {
-    id: "1",
-    title: "Going to the beach",
-    body: "Hey, I'm going to the beach today!",
-    reactions: 2,
-    userID: "user-5",
-    tags: ["beach", "fun", "sun"],
-  },
-
-  {
-    id: "2",
-    title: "New job",
-    body: "I got a new job today!",
-    reactions: 15,
-    userID: "user-12",
-    tags: ["job", "work", "career"],
-  },
-
-  {
-    id: "3",
-    title: "New car",
-    body: "I bought a new car today!",
-    reactions: 10,
-    userID: "user-8",
-    tags: ["car", "auto"],
-  },
-];
 
 export default PostListProvider;
